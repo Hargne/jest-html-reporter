d8bff6cd6d6c8775d140de64949e7fd490972230
Johan Hargne <Johan.Hargne@kindredplc.com>
2017-06-26 15:09:29 +0200

    0.4.0 - Minor restructuring of index

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const methods = require('./src/methods');
 
 module.exports = (testResult) => {
+	// Set output destination
+	const destination = methods.getOutputFilepath();
 	// Generate Report
-	methods.createReport({
-		data: testResult,
-		output: methods.getOutputFilepath(),
-	});
+	methods.createReport({ data: testResult, output: destination });
 	// Return the results as required by Jest
 	return testResult;
 };
