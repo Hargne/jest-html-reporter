dbda0e13227211e690abc3cab40d2e334073a3f1
Johan Hargne <johan.hargne@gmail.com>
2017-07-01 03:27:40 +0200

    #9 - Removed restructuring, Moved index into src

--- test/methods.spec.js
+++ test/methods.spec.js
@@ -5,10 +5,7 @@ describe('methods', () => {
 	describe('createReport ', () => {
 		it('should successfully create a test report', () => {
 			expect.assertions(1);
-			return methods.createReport({
-				data: _mockdata.jestTestData,
-				output: 'test-report.html',
-			})
+			return methods.createReport(_mockdata.jestTestData, 'test-report.html')
 			.then(response => {
 				expect(response.logMsg).toEqual('jest-html-reporter >> Report generated (test-report.html)');
 			});
@@ -29,34 +26,20 @@ describe('methods', () => {
 				expect(err).toEqual('Test data missing or malformed');
 			});
 		});
-		it('should reject the promise if the test data is not an object', () => {
-			expect.assertions(1);
-			return methods.renderHTML('invalid').catch(err => {
-				expect(err).toEqual('Test data missing or malformed');
-			});
-		});
 	});
 
 	describe('logMessage', () => {
 		it('should log a given message of an existing log type', () => {
 			// Given
-			const log = methods.logMessage({
-				type: 'success',
-				msg: 'msg',
-			});
+			const log = methods.logMessage('success', 'msg');
 			// Then
 			expect(log.logColor).toEqual('\x1b[32m');
 			expect(log.logMsg).toEqual('jest-html-reporter >> msg');
 		});
 		it('should set the log type to default if none or an invalid type was provided', () => {
 			// Given
-			const log1 = methods.logMessage({
-				msg: 'msg',
-			});
-			const log2 = methods.logMessage({
-				type: 'invalid',
-				msg: 'msg',
-			});
+			const log1 = methods.logMessage(null, 'msg');
+			const log2 = methods.logMessage('invalidType', 'msg');
 			// Then
 			expect(log1.logColor).toEqual('\x1b[37m');
 			expect(log2.logColor).toEqual('\x1b[37m');
