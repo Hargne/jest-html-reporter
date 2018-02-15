afa7a137ee56330c1ea05be08b64b53743e8f7f4
ian.he <ian.he@centrality.ai>
2018-02-15 15:36:12 +1300

    fix base on the latest code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 const methods = require('./methods');
 
 class HtmlReporter {
-    constructor(globalConfig, options) {
-        this._globalConfig = globalConfig;
-        this._options = options;
-    }
-
     onRunComplete(contexts, results) {
-        const destination = methods.getOutputFilepath();
-        // Generate Report
-        return methods.createReport(results, destination);
+		return methods.createReport(results);
     }
 }
 
